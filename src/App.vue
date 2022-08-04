8a118d4ff085948ab24a6f7086ced7a4679ecb03
riho-trm <riho.n.0304+1@gmail.com>
2022-08-04 22:11:39 +0900

    初期設定

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import "sanitize.css";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Header></Header>
   <router-view />
 </template>
 
@@ -17,18 +15,5 @@ import "sanitize.css";
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
