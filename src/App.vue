c178075438fe3b857e35694b37803223f6691b78
riho-trm <riho.n.0304+1@gmail.com>
2022-08-04 21:47:25 +0900

    リセットCSSの追加

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
+<script setup lang="ts">
+import "sanitize.css";
+</script>
+
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
