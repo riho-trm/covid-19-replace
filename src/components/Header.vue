9effbbcd99f3ce6b703356ed7ca1abbe9049a5c6
riho-trm <riho.n.0304+1@gmail.com>
2022-08-05 12:09:25 +0900

    ヘッダー作成

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="bg-red-800 text-white">
+    <div>
+      <h1 class="text-center p-4 text-6xl font-bold">COVID-19 Japan</h1>
+      <p class="text-center p-3 mb-3 text-xl font-medium">
+        新型コロナウイルス対策ダッシュボード
+      </p>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss"></style>
