f8ef8cbd3a31e74e7936415cda2623dc03e3e3af
riho-trm <riho.n.0304+1@gmail.com>
2022-08-05 18:10:03 +0900

    propsとemitを追加

--- src/components/presentation/BaseButton.vue
+++ src/components/presentation/BaseButton.vue
@@ -3,12 +3,22 @@ interface Props {
   displayName: string;
 }
 const props = defineProps<Props>();
+
+interface Emits {
+  (e: "click"): void;
+}
+const emit = defineEmits<Emits>();
+
+const handleClick = () => {
+  emit("click");
+};
 </script>
 
 <template>
   <button
     type="button"
     class="border border-gray-700 bg-gray-700 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline"
+    @click="handleClick"
   >
     {{ displayName }}
   </button>
