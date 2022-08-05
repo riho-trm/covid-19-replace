3ea18166110b2ac63e5b7676030ba9970447c72b
riho-trm <riho.n.0304+1@gmail.com>
2022-08-05 17:40:02 +0900

    テンプレ作成

---
+++ src/components/presentation/BaseButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button
+    type="button"
+    class="border border-gray-700 bg-gray-700 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline"
+  >
+    Dark
+  </button>
+</template>
