d278deb26dc6e3c0799e6e739f15ef84d7297b97
riho-trm <riho.n.0304+1@gmail.com>
2022-08-05 18:03:04 +0900

    propsの設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    // コンパイラーマクロのnot definedを消すため
+    "vue/setup-compiler-macros": true,
   },
   extends: [
     "plugin:vue/vue3-essential",
--- src/components/presentation/BaseButton.vue
+++ src/components/presentation/BaseButton.vue
@@ -1,10 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface Props {
+  displayName: string;
+}
+const props = defineProps<Props>();
+</script>
 
 <template>
   <button
     type="button"
     class="border border-gray-700 bg-gray-700 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline"
   >
-    Dark
+    {{ displayName }}
   </button>
 </template>
