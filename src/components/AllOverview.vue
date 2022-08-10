ad7532689a9a25fb09184391b93e58012b062e8f
riho-trm <riho.n.0304+1@gmail.com>
2022-08-10 16:37:11 +0900

    テーブルデザイン追加

--- src/components/AllOverview.vue
+++ src/components/AllOverview.vue
@@ -1,46 +1,90 @@
 <script setup lang="ts">
-    
+import { useStore } from "vuex";
+
+const store = useStore();
+const created = async () => {
+  try {
+    await store.dispatch("getPatientsNumber");
+    await store.dispatch("getPromptReport");
+    await store.dispatch("getBedsData");
+    await store.dispatch("getAppUrl");
+    await store.dispatch("getVentilator");
+  } catch (e) {
+    console.log(e);
+  }
+};
+created();
 </script>
 
 <template>
-<div class="overview-wrapper">
+  <div class="overview-wrapper">
     <!-- 左側の全国概況テーブル -->
-  <div class="all-overview">
-    <div class="table">
-    <table class="border-collapse border-2 border-gray-500">
-        <tr>
-            <th class="border border-gray-400 px-4 py-2 text-gray-800">現在患者数/対策病床数</th>
-            <th class="border border-gray-400 px-4 py-2 text-gray-800">現在患者数</th>
-        </tr>
-        <tr>
-            <td class="border border-gray-400 px-4 py-2">1,688%</td>
-            <td class="border border-gray-400 px-4 py-2">1,925,104%</td>
-        </tr>
-        <tr>
-            <th class="border border-gray-400 px-4 py-2 text-gray-800">累積退院者</th>
-            <th class="border border-gray-400 px-4 py-2 text-gray-800">累積退院者</th>
-        </tr>
-        <tr>
-            <td class="border border-gray-400 px-4 py-2">11,993,031人</td>
-            <td class="border border-gray-400 px-4 py-2">33,513人</td>
-        </tr>
-        <tr>
-            <th class="border border-gray-400 px-4 py-2 text-gray-800">対策病床数 </th>
-            <th class="border border-gray-400 px-4 py-2 text-gray-800">PCR検査陽性者数</th>
-        </tr>
-        <th>
-            <td class="border border-gray-400 px-4 py-2">臨床工学技士 14,378人 / 人工呼吸器 28,197台 / ECMO 1,412台 2020年2月回答 出典 一般社団法人 日本呼吸療法医学会 公益社団法人 日本臨床工学技士会</td>
-        </th>
-    </table>
+    <div class="all-overview">
+      <div class="table">
+        <table class="border-collapse border-2 border-red-800">
+          <tr>
+            <th class="border-2 border-red-800 px-4 text-sm font-normal">
+              現在患者数/対策病床数
+            </th>
+            <th class="border-2 border-red-800 px-4 text-sm font-normal">
+              現在患者数
+            </th>
+          </tr>
+          <tr>
+            <td
+              class="border-2 border-red-800 px-4 py-2 text-3xl text-white font-medium text-center bg-red-800"
+            >
+              1,688%
+            </td>
+            <td
+              class="border-2 border-red-800 px-4 py-2 text-3xl text-white font-medium text-center bg-red-800"
+            >
+              1,925,104%
+            </td>
+          </tr>
+          <tr>
+            <th class="border border-red-800 px-4 text-sm font-normal">
+              累積退院者
+            </th>
+            <th class="border-2 border-red-800 px-4 text-sm font-normal">
+              累積退院者
+            </th>
+          </tr>
+          <tr>
+            <td
+              class="border-2 border-red-800 px-4 py-2 text-3xl text-white font-medium text-center bg-red-800"
+            >
+              11,993,031人
+            </td>
+            <td
+              class="border-2 border-red-800 px-4 py-2 text-3xl text-white font-medium text-center bg-red-800"
+            >
+              33,513人
+            </td>
+          </tr>
+          <tr>
+            <th class="border-2 border-red-800 px-4 text-sm font-normal">
+              対策病床数 115,400床
+            </th>
+            <th class="border-2 border-red-800 px-4 text-sm font-normal">
+              PCR検査陽性者数 14,429,600人
+            </th>
+          </tr>
+          <tr>
+            <td colspan="2" class="border border-red-800 px-4 py-2 text-center">
+              <p>
+                臨床工学技士 14,378人 / 人工呼吸器 28,197台 / ECMO 1,412台<br />2020年2月回答
+                出典 一般社団法人 日本呼吸療法医学会 公益社団法人
+                日本臨床工学技士会
+              </p>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="update-date"></div>
     </div>
-    <div class="update-date">
-    </div>
+    <!-- 右側の県別概況の表、v-forで書く -->
+    <div class="overview-by-prefecture"></div>
   </div>
-  <!-- 右側の県別概況の表、v-forで書く -->
-  <div class="overview-by-prefecture"> 
-  </div>
-  </div>
-  <div class="supplement">
-
-  </div>
+  <div class="supplement"></div>
 </template>
