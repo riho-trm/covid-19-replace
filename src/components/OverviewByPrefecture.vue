6b5e6a66cdc759f1b25d0842ab52a3c0e5a339cb
riho-trm <riho.n.0304+1@gmail.com>
2022-08-16 12:25:30 +0900

    変数用意

---
+++ src/components/OverviewByPrefecture.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { useStore } from "vuex";
+
+const store = useStore();
+
+interface Props {
+  promptChecked: boolean;
+  prefCode: string;
+  prefName: string;
+  beds: number;
+  bedRate: number;
+  bedsUPdate: string;
+}
+defineProps<Props>();
+
+const graphData = [
+  {
+    currentPatients: 0,
+    deaths: 0,
+  },
+];
+const displayData = {
+  prefJp: "",
+  patients: 0,
+  currentPatients: 0,
+  exits: 0,
+  deaths: 0,
+  source: "",
+  sourceUrl: "",
+  lastUpdate: "",
+  url: "",
+  appUrl: "",
+  clinicalEngineer: 0,
+  ventilator: 0,
+  ecmo: 0,
+};
+</script>
+
+<template>
+  <div></div>
+</template>
